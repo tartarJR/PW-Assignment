22324f5be84a0998689556ac5d5fc3d114da7ba5
tartarJR <mustafaogun.ozturk@gmail.com>
2019-03-05 22:30:55 +0200

    Move api url prefix to app properties

--- src/main/java/com/planetway/assignment/controller/HelloWorldController.java
+++ src/main/java/com/planetway/assignment/controller/HelloWorldController.java
@@ -1,11 +1,9 @@
 package com.planetway.assignment.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api")
 public class HelloWorldController {
 
     @GetMapping("/hello")
