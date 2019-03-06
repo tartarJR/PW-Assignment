ce679945d688e37aaecde4dbf213f961b86f71b1
tartarJR <mustafaogun.ozturk@gmail.com>
2019-03-06 03:29:00 +0200

    Add implementation of Employee service and repository

--- src/main/java/com/planetway/assignment/repository/EmployeeRepository.java
+++ src/main/java/com/planetway/assignment/repository/EmployeeRepository.java
@@ -1,7 +1,9 @@
 package com.planetway.assignment.repository;
 
+import com.planetway.assignment.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface EmployeeRepository {
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 }
---
+++ src/main/java/com/planetway/assignment/service/EmployeeService.java
@@ -0,0 +1,21 @@
+package com.planetway.assignment.service;
+
+import com.planetway.assignment.model.Employee;
+import com.planetway.assignment.repository.EmployeeRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class EmployeeService {
+
+    private final EmployeeRepository employeeRepository;
+
+    public EmployeeService(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    public List<Employee> getEmployeeList() {
+        return employeeRepository.findAll();
+    }
+}
