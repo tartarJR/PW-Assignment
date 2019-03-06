b88465668e95b06dda0107d228cea52b27d99df0
tartarJR <mustafaogun.ozturk@gmail.com>
2019-03-06 02:14:17 +0200

    Add controller advice exception handler

---
+++ src/main/java/com/planetway/assignment/exception/RestExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.planetway.assignment.exception;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+
+@ControllerAdvice
+public class RestExceptionHandler {
+    @ExceptionHandler({InvalidParameterException.class})
+    public final ResponseEntity<ApiErrorResponse> handleAllExceptions(Exception ex, WebRequest request) {
+        ApiErrorResponse apiErrorResponse = new ApiErrorResponse(ex.getMessage());
+        return new ResponseEntity<>(apiErrorResponse, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler({MethodArgumentTypeMismatchException.class})
+    public final ResponseEntity<ApiErrorResponse> handleAllExceptions(MethodArgumentTypeMismatchException ex, WebRequest request) {
+        ApiErrorResponse apiErrorResponse = new ApiErrorResponse("Make sure you entered  digits for number fields.");
+        return new ResponseEntity<>(apiErrorResponse, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
