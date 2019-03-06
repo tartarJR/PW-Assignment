7bf721b5b867fc55367c8d504e654721686a27e1
tartarJR <mustafaogun.ozturk@gmail.com>
2019-03-06 02:41:11 +0200

    Update exception handler to handle jackson not readable error

--- src/main/java/com/planetway/assignment/exception/RestExceptionHandler.java
+++ src/main/java/com/planetway/assignment/exception/RestExceptionHandler.java
@@ -3,6 +3,7 @@ package com.planetway.assignment.exception;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
@@ -11,13 +12,13 @@ import org.springframework.web.method.annotation.MethodArgumentTypeMismatchExcep
 @ControllerAdvice
 public class RestExceptionHandler {
     @ExceptionHandler({InvalidParameterException.class})
-    public final ResponseEntity<ApiErrorResponse> handleAllExceptions(Exception ex, WebRequest request) {
+    public final ResponseEntity<ApiErrorResponse> handleAllExceptions(InvalidParameterException ex, WebRequest request) {
         ApiErrorResponse apiErrorResponse = new ApiErrorResponse(ex.getMessage());
         return new ResponseEntity<>(apiErrorResponse, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
-    @ExceptionHandler({MethodArgumentTypeMismatchException.class})
-    public final ResponseEntity<ApiErrorResponse> handleAllExceptions(MethodArgumentTypeMismatchException ex, WebRequest request) {
+    @ExceptionHandler({MethodArgumentTypeMismatchException.class, HttpMessageNotReadableException.class})
+    public final ResponseEntity<ApiErrorResponse> handleAllExceptions(Exception ex, WebRequest request) {
         ApiErrorResponse apiErrorResponse = new ApiErrorResponse("Make sure you entered  digits for number fields.");
         return new ResponseEntity<>(apiErrorResponse, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
